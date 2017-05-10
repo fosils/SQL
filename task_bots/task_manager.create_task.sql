580486d507508acc161e70e53ea596d468101694
Alexey <aignatyuk@mail.ru>
2017-05-10 13:09:32 +0500

    fixed worker_initials in task_manager.create_task

--- task_bots/task_manager.create_task.sql
+++ task_bots/task_manager.create_task.sql
@@ -11,12 +11,14 @@ declare
 	deadline timestamp;
 	task_giver varchar;
 	worker_email varchar;
+	_task_giver varchar;
 begin
 	accept_unique_short_id = task_manager.smart_link_unique_short_id();
 	reject_unique_short_id = task_manager.smart_link_unique_short_id();
 	completed_unique_short_id = task_manager.smart_link_unique_short_id();
 	completed_failure_unique_short_id = task_manager.smart_link_unique_short_id();
 	task_unique_short_id = task_manager.task_unique_short_id();
+	select worker_initials into _task_giver from task_manager.freelancers where email = gui.get_current_user();
 	deadline = null;--now() + interval '3 days';
 	--execute('select current_setting(''session.username'') into task_giver;');
 	select email into worker_email from freelancers where freelancers.worker_initials = the_worker_initials;
@@ -37,7 +39,9 @@ begin
 			hstore('email_subject', 'You have been given a new task:') || hstore('sendFrom', _send_from) || hstore('mergedHtml.sendFrom', _send_from) || hstore('html.sendFrom', _send_from) || task_description_variables || hstore('deadline', deadline::text) || hstore('cell_column', cell_column) || hstore('from', "gui"."get_current_user"()) || hstore('worker_email', worker_email) || hstore('reject_link', concat('ebogholderen.dk/smart_links/smart_links.php?hash=', reject_unique_short_id)) || hstore('accept_link', concat('ebogholderen.dk/smart_links/smart_links.php?hash=', accept_unique_short_id)) || hstore('completed_link', concat('ebogholderen.dk/smart_links/smart_links.php?hash=', completed_unique_short_id)) || hstore('completed_with_failure_link', concat('ebogholderen.dk/smart_links/smart_links.php?hash=', completed_failure_unique_short_id)),
 			task_unique_short_id,
 			replace(ultradox, '/edit/', '/run/'),
-			TRUE, cell_table, cell_column, cell_id, from_date, until_date, (select id from column_task_description where column_name = cell_column), gui.get_current_user());
+			TRUE, cell_table, cell_column, cell_id, from_date, until_date, (select id from column_task_description where column_name = cell_column),
+			_task_giver
+			);
 
 	INSERT INTO smart_links (hash, redirect_url, QUERY) values 
 	(accept_unique_short_id, 'http://tinyurl.com/h2hgnbl', concat('select task_manager.update_task_and_colors_from_smartlink(''', task_unique_short_id, ''', ''accepted'',''', cell_table, ''',''', cell_column, ''',''', cell_id, ''')'));
@@ -49,3 +53,4 @@ begin
 	(completed_failure_unique_short_id, 'http://tinyurl.com/h2hgnbl', concat('select task_manager.update_task_and_colors_from_smartlink(''', task_unique_short_id, ''', ''completed_failure'',''', cell_table, ''',''', cell_column, ''',''', cell_id, ''')'));
 end;
 $function$
+
