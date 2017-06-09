fdc5f376b93df5d89084d6b3fe2754c7bb40cedc
Alexey <aignatyuk@mail.ru>
2017-06-09 16:47:48 +0500

    renamed columns according the naming convention

--- automatic-spiir/external_data.spiir_raw_data.sql
+++ automatic-spiir/external_data.spiir_raw_data.sql
@@ -1,9 +1,9 @@
-create table external_data.spiir_raw_data(id serial, customer_id int , transaction_id text, AccountId text,
-							AccountName text, AccountType text, Date text, Description text,
-							OriginalDescription text, MainCategoryId text, MainCategoryName text, CategoryId text, 
-							CategoryName text, CategoryType text, ExpenseType text, Amount text,Balance text, CounterEntryId text, Comment text, Tags text, Extraordinary text, SplitGroupId text, CustomDate text);
+create table external_data.spiir_raw_data(id serial, customer_id int , transaction_id text, account_id text,
+							account_name text, account_type text, date text, description text,
+							original_description text, main_category_id text, main_category_name text, category_id text, 
+							category_name text, category_type text, expense_type text, amount text,balance text, counter_entry_id text, comment text, tags text, extraordinary text, split_group_id text, custom_date text);
 
-create table external_data.spiir_raw_data_temp ( transaction_id text, AccountId text,
-							AccountName text, AccountType text, Date text, Description text,
-							OriginalDescription text, MainCategoryId text, MainCategoryName text, CategoryId text, 
-							CategoryName text, CategoryType text, ExpenseType text, Amount text,Balance text, CounterEntryId text, Comment text, Tags text, Extraordinary text, SplitGroupId text, CustomDate text);
+create table external_data.spiir_raw_data_temp ( transaction_id text, account_id text,
+							account_name text, account_type text, date text, description text,
+							original_description text, main_category_id text, main_category_name text, category_id text, 
+							category_name text, category_type text, expense_type text, amount text,balance text, counter_entry_id text, comment text, tags text, extraordinary text, split_group_id text, custom_date text);
