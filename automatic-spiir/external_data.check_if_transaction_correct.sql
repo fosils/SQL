6e7105da04323e7c14a4f486730a4b9ba7b7ada4
Alexey <aignatyuk@mail.ru>
2017-06-13 12:16:08 +0500

    changed float to numeric

--- automatic-spiir/external_data.check_if_transaction_correct.sql
+++ automatic-spiir/external_data.check_if_transaction_correct.sql
@@ -3,13 +3,15 @@ CREATE OR REPLACE FUNCTION external_data.check_if_transaction_correct(_transacti
  LANGUAGE plpgsql
 AS $function$
 declare
- last_balance float;
- current_balance float;
- _amount float;
+ last_balance numeric;
+ current_balance numeric;
+ _amount numeric;
+ last_transaction_id bigint;
 begin
-	select replace(balance,',','.')::float into current_balance from external_data.spiir_raw_data where transaction_id::bigint = (_transaction_id);
-	select replace(balance,',','.')::float into last_balance from external_data.spiir_raw_data where transaction_id::bigint = (_transaction_id-1);
-	select replace(amount,',','.')::float into _amount from external_data.spiir_raw_data where transaction_id::bigint = (_transaction_id);
+	select max(external_data.spiir_raw_data.transaction_id ::bigint) into last_transaction_id from external_data.spiir_raw_data where external_data.spiir_raw_data.transaction_id::bigint<_transaction_id; 
+	select replace(balance,',','.')::numeric into current_balance from external_data.spiir_raw_data where transaction_id::bigint = (_transaction_id);
+	select replace(balance,',','.')::numeric into last_balance from external_data.spiir_raw_data where transaction_id::bigint = (last_transaction_id);
+	select replace(amount,',','.')::numeric into _amount from external_data.spiir_raw_data where transaction_id::bigint = (_transaction_id);
 	if current_balance = (last_balance+_amount) then
 		return true;
 	else 
