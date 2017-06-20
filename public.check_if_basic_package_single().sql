13579dbd930d87c9a1d2067bb7f6d7ec4608470c
Alexey <aignatyuk@mail.ru>
2017-06-20 21:52:33 +0500

    changed compare method

--- public.check_if_basic_package_single().sql
+++ public.check_if_basic_package_single().sql
@@ -3,26 +3,22 @@ create or replace function public.check_if_basic_package_single()
  	LANGUAGE plpgsql
 AS $function$
 declare
-	_from_date date;
-	_until_date date;
+	_service_from date;
+	_service_until date;
 	_package text;
 	_current_package_count int;
 	_customer_id int;
 	_date_diff int;
 begin
-	select new.customer_id, new.service_from, new.service_until,new.package into _customer_id,_from_date,_until_date,_package;
+	select new.customer_id, new.service_from, new.service_until,new.package into _customer_id,_service_from,_service_until,_package;
 	if ((select extra_service from products where short_name=_package)=false) then 
-		select DATE_PART('year', get_start_date_of_current_financial_year(_customer_id)::date) - DATE_PART('year', _from_date::date) into _date_diff;
- 		select count(package)  into _current_package_count from customer_payments 
+		select count(package)  into _current_package_count from customer_payments 
  		join products on (products.short_name = package )
  		where customer_payments.customer_id = _customer_id
-			and service_from >= (get_start_date_of_current_financial_year(_customer_id) - interval '1 year'*_date_diff)  
-			and (service_until <= get_start_date_of_current_financial_year(_customer_id)- interval '1 year'*_date_diff + interval '1 year')
-			and products.extra_service = false;
-	
-	
+			and ( _service_from between service_from and service_until)    
+			and products.extra_service = false;	
 		if (_current_package_count>0 ) then 
-			RAISE EXCEPTION 'There is already a basic package in this period';
+			RAISE EXCEPTION 'There is already a basic package in this period.';
 		end if;
 	end if ;
 	return new;
