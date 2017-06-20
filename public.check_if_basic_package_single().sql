7fbdabe13544d7f947eda65a268fc6cf3e36ef4e
Alexey <aignatyuk@mail.ru>
2017-06-20 16:18:36 +0500

    changed compare order

--- public.check_if_basic_package_single().sql
+++ public.check_if_basic_package_single().sql
@@ -11,20 +11,21 @@ declare
 	_date_diff int;
 begin
 	select new.customer_id, new.service_from, new.service_until,new.package into _customer_id,_from_date,_until_date,_package;
-	select DATE_PART('year', get_start_date_of_current_financial_year(_customer_id)::date) - DATE_PART('year', _from_date::date) into _date_diff;
- 	select count(package)  into _current_package_count from customer_payments 
+	if ((select extra_service from products where short_name=_package)=false) then 
+		select DATE_PART('year', get_start_date_of_current_financial_year(_customer_id)::date) - DATE_PART('year', _from_date::date) into _date_diff;
+ 		select count(package)  into _current_package_count from customer_payments 
  		join products on (products.short_name = package )
  		where customer_payments.customer_id = _customer_id
 			and service_from >= (get_start_date_of_current_financial_year(_customer_id) - interval '1 year'*_date_diff)  
 			and (service_until <= get_start_date_of_current_financial_year(_customer_id)- interval '1 year'*_date_diff + interval '1 year')
 			and products.extra_service = false;
 	
-	if ((select extra_service from products where short_name=_package)=false)
-		and (_current_package_count>0 )
-	then
-		RAISE EXCEPTION 'There is already a basic package in this period';
-	end if;
 	
+		if (_current_package_count>0 ) then 
+		then
+			RAISE EXCEPTION 'There is already a basic package in this period';
+		end if;
+	end if ;
 	return new;
 end;
 $function$;
