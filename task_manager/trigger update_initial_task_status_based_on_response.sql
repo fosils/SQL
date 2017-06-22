e812b50d520b5ec38668be93e4b1ff9520098b08
Alexey <aignatyuk@mail.ru>
2017-06-22 16:16:29 +0500

    if task has initial task then status of the task (not the initial task) set to completed on response change

--- task_manager/trigger update_initial_task_status_based_on_response.sql
+++ task_manager/trigger update_initial_task_status_based_on_response.sql
@@ -2,9 +2,15 @@ CREATE OR REPLACE FUNCTION task_manager.update_initial_task_status_based_on_resp
  RETURNS trigger
  LANGUAGE plpgsql
 AS $function$
+declare 
+ initial_task_id int;
 begin
+	initial_task_id = task_manager.get_initial_task_id(new.task_id);
 	update task_manager.tasks set task_status = (select new_original_task_status from  task_manager.possible_responses where id = (select possible_response_id from task_manager.task_responses where task_id =new.task_id ))  
 	where id = task_manager.get_initial_task_id(new.task_id);
+	if initial_task_id <> new.task_id then
+		update task_manager.tasks set task_status = 'completed' where id = new.task_id;
+	end if;
 	return new;
 end
 $function$;
