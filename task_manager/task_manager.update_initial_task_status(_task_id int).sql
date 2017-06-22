0445541471040882b2389f8a2b796ab16690f066
Alexey <aignatyuk@mail.ru>
2017-06-22 14:16:34 +0500

    renamed from 'parent' to 'initial'

--- task_manager/task_manager.update_initial_task_status(_task_id int).sql
+++ task_manager/task_manager.update_initial_task_status(_task_id int).sql
@@ -7,7 +7,7 @@ declare
 begin
  	_task_status = (select new_original_task_status from  task_manager.possible_responses where id = (select possible_response_id from task_manager.task_responses where task_id =_task_id ));
 	update task_manager.tasks set task_status = _task_status  
-	where id = task_manager.get_task_parent(_task_id);
+	where id = task_manager.get_initial_task(_task_id);
 	return _task_status;
 end;
 $function$;
