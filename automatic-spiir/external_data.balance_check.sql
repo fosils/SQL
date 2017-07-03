be69cf0edef724dc9531897941fbcabc7f5ae10b
Alexey <aignatyuk@mail.ru>
2017-07-03 14:26:54 +0500

    some changes

--- automatic-spiir/external_data.balance_check.sql
+++ automatic-spiir/external_data.balance_check.sql
@@ -1,5 +1,5 @@
-
-CREATE OR REPLACE FUNCTION external_data.balance_check(_transaction_id bigint, _account_id bigint)
+--drop FUNCTION external_data.balance_check(_transaction_id bigint, _account_id bigint)
+CREATE OR REPLACE FUNCTION external_data.balance_check(_transaction_id bigint, _account_id bigint,_date date)
  RETURNS bool
  LANGUAGE plpgsql
 AS $function$
@@ -9,10 +9,18 @@ declare
  _amount numeric;
  last_transaction_id bigint;
 begin
-	select max(external_data.spiir_raw_data.transaction_id ::bigint) into last_transaction_id from external_data.spiir_raw_data where external_data.spiir_raw_data.transaction_id::bigint<_transaction_id and external_data.spiir_raw_data.account_id::bigint =_account_id; 
+	select max(external_data.spiir_raw_data.transaction_id ::bigint) into last_transaction_id from external_data.spiir_raw_data 
+		where 
+			external_data.spiir_raw_data.transaction_id::bigint<_transaction_id 
+			and external_data.spiir_raw_data.account_id::bigint =_account_id
+			and external_data.spiir_raw_data.date::date<=_date; 
+	raise notice '%',last_transaction_id;
 	select replace(balance,',','.')::numeric into current_balance from external_data.spiir_raw_data where transaction_id::bigint = (_transaction_id);
+	raise notice '%',current_balance;
 	select replace(balance,',','.')::numeric into last_balance from external_data.spiir_raw_data where transaction_id::bigint = (last_transaction_id);
+	raise notice '%',last_balance;
 	select replace(amount,',','.')::numeric into _amount from external_data.spiir_raw_data where transaction_id::bigint = (_transaction_id);
+	raise notice '%',_amount;
 	if current_balance = (last_balance+_amount) then
 		return true;
 	else 
