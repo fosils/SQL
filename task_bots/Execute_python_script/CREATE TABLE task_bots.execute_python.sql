7be17326c2117a9721ba765dfe161553eb1545df
Alexey <aignatyuk@mail.ru>
2017-09-26 15:12:59 +0500

    changed id to serial

--- task_bots/Execute_python_script/CREATE TABLE task_bots.execute_python.sql
+++ task_bots/Execute_python_script/CREATE TABLE task_bots.execute_python.sql
@@ -1,5 +1,5 @@
 CREATE TABLE task_bots.execute_python (
-	id int4 NOT NULL DEFAULT nextval('task_bots.execute_python_id_seq'::regclass),
+	id serial,
 	python_script varchar(4000) NULL,
 	parameters varchar(4000) NULL,
 	schedule_time timestamp NULL,
